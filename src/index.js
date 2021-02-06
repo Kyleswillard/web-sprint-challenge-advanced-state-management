b7ad4671843135d3f36bafc90f6faba1249df09e
Kyle Willard <kyleswillard@gmail.com>
2021-02-05 18:08:52 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-import "./index.css";
-import App from "./App";
+import reducers from './reducers'
 
-const { worker } = require('./mocks/browser');
-worker.start();
+import './index.css'
+import App from './App'
 
-const rootElement = document.getElementById("root");
+const { worker } = require('./mocks/browser')
+worker.start()
+const store = createStore(reducers, applyMiddleware(thunk))
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
     rootElement
-);
+)
 
 //Task List:
 //1. Add in all necessary components and libary methods.
